24f75fd576037a64355df796cab07c58300c784c
Lucas Diello <lucasoliveiradiello@gmail.com>
2024-08-13 13:39:29 -0300

    add url to production

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const url = import.meta.env.VITE_API_URL; 
-const urlLocal = import.meta.env.VITE_API_URL_LOCAL;
-console.log(url);
 const apiRequest = axios.create({
-    baseURL: urlLocal,
+    baseURL: url,
     withCredentials: true,
 });
 
